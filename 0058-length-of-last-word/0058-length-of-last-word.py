3dc37ad3a439ac0484b949129d7f50201d1db6c7
Rahul Kumar <rahulkr18499@gmail.com>
2026-09-15 16:53:37 +0530

    Time: 0 ms (100%), Space: 19.5 MB (16.47%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -3,11 +3,11 @@ class Solution:
         
         i = len(s) - 1
         
-        # Step 1: skip trailing spaces
+    
         while i >= 0 and s[i] == ' ':
             i -= 1
         
-        # Step 2: count last word
+        
         length = 0
         while i >= 0 and s[i] != ' ':
             length += 1
